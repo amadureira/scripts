5353d9ba3388e35cb220bfd1fd676b7cd1045d63
amadureira <andersonmadureira@gmail.com>
2019-10-13 13:00:09 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+apt-get install apt-transport-https
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main"
+apt update
+apt -y  install default-jre
+apt -y  install elasticsearch
+vim /etc/elasticsearch/elasticsearch.yml
+   discovery.type: single-node
+   network.host: 0.0.0.0
+systemctl start elasticsearch
+
+wget https://raw.githubusercontent.com/amadureira/scripts/master/gera.sh
+chmod 0755 gera.sh
+apt install bc
+./gera.sh $(date +%s | sed 's;^\([0-9]\+\);\1-21600;g'  |bc)  $(date +%s)  60 host-1 'uso de cpu'  99 2
+./gera.sh $(date +%s | sed 's;^\([0-9]\+\);\1-21600;g'  |bc)  $(date +%s)  60 host-2 'uso de cpu'  99 2
+./gera.sh $(date +%s | sed 's;^\([0-9]\+\);\1-21600;g'  |bc)  $(date +%s)  60 host-3 'uso de cpu'  99 2
+
+wget https://dl.grafana.com/oss/release/grafana_6.4.2_amd64.deb
+dpkg -i grafana_6.4.2_amd64.deb
+systemctl start grafana-server
